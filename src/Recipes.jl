5c37f2de40c577d3dfde3de7d5ce62e34e1bd64b
singularitti <singularitti@outlook.com>
2025-03-30 14:52:01 -0600

    Add `show` for `TraceEvent`

--- src/Recipes.jl
+++ src/Recipes.jl
@@ -111,4 +111,13 @@ function load_trace_csv(filepath)
     end
 end
 
+function Base.show(io::IO, ::MIME"text/plain", obj::TraceEvent)
+    summary(io, obj)
+    println(io)
+    for name in propertynames(obj)
+        println(io, "  ", name, ": ", getfield(obj, name))
+    end
+    return nothing
+end
+
 end
