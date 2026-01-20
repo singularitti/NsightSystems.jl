91602db5800cd4fa67069b401af159d4f53a0bc6
singularitti <singularitti@outlook.com>
2026-01-19 20:05:06 -0600

    Fix `show` for `TraceEvent`

--- src/Recipes/show.jl
+++ src/Recipes/show.jl
@@ -1,8 +1,9 @@
 function Base.show(io::IO, ::MIME"text/plain", obj::TraceEvent)
     summary(io, obj)
-    println(io)
+    println(io, '(')
     for name in propertynames(obj)
-        println(io, "  ", name, ": ", getfield(obj, name))
+        println(io, "  ", name, " = ", getfield(obj, name), ',')
     end
+    println(io, ')')
     return nothing
 end
